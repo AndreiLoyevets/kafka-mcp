51401f4c78eafc4bfee3c6ad22c1cb44155e0d25
andreiloyevets <aloievets@conductor.com>
2025-12-25 13:09:42 +0000

    added test MCP server communicating via SSE

--- kafka-mcp-server/src/main/java/com/aloievets/ai/mcp/kafka/Application.java
+++ kafka-mcp-server/src/main/java/com/aloievets/ai/mcp/kafka/Application.java
@@ -1,25 +1,12 @@
 package com.aloievets.ai.mcp.kafka;
 
-import com.aloievets.ai.mcp.kafka.client.KafkaStatusViewer;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
-    private final KafkaStatusViewer kafkaStatusViewer;
+public class Application {
 
-    public Application(final KafkaStatusViewer kafkaStatusViewer) {
-        this.kafkaStatusViewer = kafkaStatusViewer;
-    }
-
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Override
-    public void run(final String... args) throws Exception {
-        System.out.println(kafkaStatusViewer.listTopics());
     }
 }
