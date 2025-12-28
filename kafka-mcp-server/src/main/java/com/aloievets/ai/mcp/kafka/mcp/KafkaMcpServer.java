47849da9c9b3c64ec4e4e5ece4bba020506375f5
andreiloyevets <aloievets@conductor.com>
2025-12-28 12:54:51 +0000

    Claude-MCP server-Kafka setup

---
+++ kafka-mcp-server/src/main/java/com/aloievets/ai/mcp/kafka/mcp/KafkaMcpServer.java
@@ -0,0 +1,26 @@
+package com.aloievets.ai.mcp.kafka.mcp;
+
+import java.util.Set;
+
+import com.aloievets.ai.mcp.kafka.client.KafkaStatusViewer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springaicommunity.mcp.annotation.McpTool;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaMcpServer {
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaMcpServer.class);
+    private final KafkaStatusViewer kafkaStatusViewer;
+
+    public KafkaMcpServer(final KafkaStatusViewer kafkaStatusViewer) {
+        this.kafkaStatusViewer = kafkaStatusViewer;
+    }
+
+    @McpTool(title = "List topics", description = "List Kafka topics in my cluster")
+    public Set<String> listTopics() {
+        LOG.debug("Requested to list topics");
+        return kafkaStatusViewer.listTopics();
+    }
+}
